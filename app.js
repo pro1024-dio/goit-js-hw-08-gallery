19b032cc3b4b6b51f6198a9e9c0861d2237a5a4c
Ivan Dernovyi <idernovoy@gmail.com>
2021-08-01 22:01:24 +0300

    task
    Execute task

--- app.js
+++ app.js
@@ -63,3 +63,97 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const ISOPEN_CLASS_NAME = 'is-open';
+const gallery = document.querySelector("ul.gallery.js-gallery");
+let currentImage;
+
+const createTamplate = element => {
+  return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${element.original}"
+  >
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</li>`;
+};
+
+const handlerClick = event => {
+  const elm = event.target;
+  const curElm = event.currentTarget;
+  const lightbox = document.querySelector("div.lightbox");
+  const lightboxImg = document.querySelector(".lightbox__image");
+
+  function closeLightbox() {
+    lightbox.classList.remove(ISOPEN_CLASS_NAME);
+    lightboxImg.src = "";
+    lightboxImg.alt = "";
+    lightbox.removeEventListener('click', handlerClick);
+    document.removeEventListener('keyup', handlerClick);
+  };
+
+  function showBigImage(elm) {
+    lightboxImg.src = elm.dataset.source;
+    lightboxImg.alt = elm.alt;
+  };
+
+  event.stopImmediatePropagation();
+  if (curElm.nodeName === "A") {
+    event.preventDefault();
+  };
+
+  if (event.type === 'keyup' && event.code === 'Escape') closeLightbox();
+
+  if (elm.nodeName === "BUTTON") {
+    switch (elm.dataset.action) {
+      case "close-lightbox":
+        closeLightbox();
+        break;
+      case "left-lightbox":
+        currentImage = currentImage.parentElement.parentElement.previousElementSibling.querySelector("." + currentImage.className);
+        showBigImage(currentImage);
+        break;
+      case "right-lightbox":
+        currentImage = currentImage.parentElement.parentElement.nextElementSibling.querySelector("." + currentImage.className);
+        showBigImage(currentImage);
+        break;
+    };
+  };
+
+  if (elm.nodeName === "DIV" && elm.classList.contains("lightbox__overlay")) closeLightbox();
+  if (elm.classList.contains("lightbox__image") || elm.classList.contains("lightbox__content")) {
+    closeLightbox();
+    return;
+    };
+
+  if (elm.nodeName === "IMG") {
+  
+    //відображаємо модальне вікно та ініціалізуємо його елементи
+    lightbox.classList.add(ISOPEN_CLASS_NAME);
+    lightbox.addEventListener('click', handlerClick);
+    document.addEventListener('keyup', handlerClick);
+
+    //відображаємо поточне зображення
+    showBigImage(elm);
+
+    //зберігаємо поточне відкрите зображення
+    currentImage = elm;
+    
+  };
+};
+
+
+const markupArray = galleryItems.map(elm => createTamplate(elm));
+gallery.insertAdjacentHTML('afterbegin', markupArray.join(""));
+
+gallery.addEventListener('click', handlerClick);
+gallery.addEventListener('mousedown', handlerClick);
+gallery.addEventListener('mouseup', handlerClick);
+
+
